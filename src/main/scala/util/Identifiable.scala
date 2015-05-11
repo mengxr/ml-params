72180e519ecfbccc2f7cf41035681b6e77bf592a
Xiangrui Meng <meng@databricks.com>
2015-05-11 12:31:16 -0700

    update Identifiable

--- src/main/scala/util/Identifiable.scala
+++ src/main/scala/util/Identifiable.scala
@@ -6,7 +6,17 @@ trait Identifiable {
 
   protected val simpleName: String = this.getClass.getSimpleName
 
-  final val uid: String = simpleName + "_" + UUID.randomUUID().toString.take(8)
+  private var _uid: String = simpleName + "_" + UUID.randomUUID().toString.take(8)
+
+  /**
+   * @return uid
+   */
+  final def uid: String = _uid
 
   override def toString: String = uid
+
+  protected final def setUID(uid: String): this.type = {
+    this._uid = uid
+    this
+  }
 }
